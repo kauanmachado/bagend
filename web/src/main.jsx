ed2a9d4711a3618d7aaaf1312df82832ee24f26e
kauanmachado <kauan.smachado@gmail.com>
2023-06-02 17:25:23 -0300

    att routes

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,9 +5,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import router from "./routes";
 import { RouterProvider } from "react-router-dom";
+import Routers from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Routers/>
   </React.StrictMode>
 );
--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,6 +1,10 @@
 import {
   RouterProvider,
   createBrowserRouter,
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
 import Autenticado from "./auth";
 import Home from "./pages/Home";
@@ -20,12 +24,11 @@ import CliGeral from "./pages/PainelCliente/CliGeral";
 import CliAgendas from "./pages/PainelCliente/CliAgendas";
 import CliEditarDados from "./pages/PainelCliente/CliEditarDados";
 
-
 // const PrivateRoute = ({ component: Component, ...rest }) => {
 //     return (
 //       <Route
 //       {...rest}
-//       render={(props) => 
+//       render={(props) =>
 //         Autenticado ? (
 //           <Component {...props} />
 //         ) : (
@@ -36,71 +39,92 @@ import CliEditarDados from "./pages/PainelCliente/CliEditarDados";
 //     )
 // };
 
-const router =  createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "login-cliente",
-    element: <LoginCliente />,
-  },
-  {
-    path: "login-barbearia",
-    element: <LoginBarbearia />,
-  },
-  {
-    path: "cadastro-cliente",
-    element: <CadastroCliente />,
-  },
-  {
-    path: "cadastro-barbearia",
-    element: <CadastroBarbearia />,
-  },
-  {
-    path: "perfil-cliente",
-    element: <PerfilCliente />,
-  },
-  {
-    path: "barbearias",
-    element: <Barbearias />,
-  },
-  {
-    path: "painel-barbearia",
-    element: <Geral />,
-  },
-  {
-    path: "painel-barbearia/agendas",
-    element: <BrbAgendas />,
-  },
-  {
-    path: "painel-barbearia/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "painel-barbearia/cortes-estilos",
-    element: <CortesEstilos />,
-  },
-  {
-    path: "painel-barbearia/alterar-dados",
-    element: <BrbEditarDados />,
-  },
-  {
-    path: "painel-barbearia/cortes-estilos/adicionar-corteestilo",
-    element: <AdicionarCorteEstilo />,
-  },
-  {
-    path: "painel-cliente",
-    element: <CliGeral />,
-  },
-  {
-    path: "painel-cliente/agendas",
-    element: <CliAgendas />,
-  },
-  {
-    path: "painel-cliente/alterar-dados",
-    element: <CliEditarDados />,
-  },
-])
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login-cliente" element={<LoginCliente />} />
+        <Route path="cadastro-cliente" element={<CadastroCliente />} />
+        <Route path="login-barbearia" element={<LoginBarbearia />} />
+        <Route path="cadastro-barbearia" element={<CadastroBarbearia />} />
+        <Route path="painel-cliente" element={<CliGeral />} />
+        <Route path="painel-cliente/agendas" element={<CliAgendas />} />
+        <Route path="painel-cliente/alterar-dados" element={<CliEditarDados />} />
+        <Route path="painel-barbearia" element={<Geral />} />
+        <Route path="painel-barbearia/agendas" element={<BrbAgendas />} />
+        <Route path="painel-barbearia/cortes-estilos" element={<CortesEstilos />} />
+        <Route path="painel-barbearia/alterar-dados" element={<BrbEditarDados />} />
+        <Route path="painel-barbearia/cortes-estilos/adicionar-corteestilo" element={<AdicionarCorteEstilo />} />
+        <Route path="/barbearias" element={<Barbearias />} />       
+      </Routes>
+    </BrowserRouter>
+  );
+};
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "login-cliente",
+//     element: <LoginCliente />,
+//   },
+//   {
+//     path: "login-barbearia",
+//     element: <LoginBarbearia />,
+//   },
+//   {
+//     path: "cadastro-cliente",
+//     element: <CadastroCliente />,
+//   },
+//   {
+//     path: "cadastro-barbearia",
+//     element: <CadastroBarbearia />,
+//   },
+//   {
+//     path: "perfil-cliente",
+//     element: <PerfilCliente />,
+//   },
+//   {
+//     path: "barbearias",
+//     element: <Barbearias />,
+//   },
+//   {
+//     path: "painel-barbearia",
+//     element: <Geral />,
+//   },
+//   {
+//     path: "painel-barbearia/agendas",
+//     element: <BrbAgendas />,
+//   },
+//   {
+//     path: "painel-barbearia/dashboard",
+//     element: <Dashboard />,
+//   },
+//   {
+//     path: "painel-barbearia/cortes-estilos",
+//     element: <CortesEstilos />,
+//   },
+//   {
+//     path: "painel-barbearia/alterar-dados",
+//     element: <BrbEditarDados />,
+//   },
+//   {
+//     path: "painel-barbearia/cortes-estilos/adicionar-corteestilo",
+//     element: <AdicionarCorteEstilo />,
+//   },
+//   {
+//     path: "painel-cliente",
+//     element: <CliGeral />,
+//   },
+//   {
+//     path: "painel-cliente/agendas",
+//     element: <CliAgendas />,
+//   },
+//   {
+//     path: "painel-cliente/alterar-dados",
+//     element: <CliEditarDados />,
+//   },
+// ])
 
-export default router
+export default Routers;
